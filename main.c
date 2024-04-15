632649afdf77f65ca582f56109f23e5730cc1d1a
jayarc7 <151756042+jayarc7@users.noreply.github.com>
2024-04-15 16:57:28 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Team_1 {
+    char name[25];
+    int score;
+};
+struct Team_2 {
+    char name[25];
+    int score;
+};
+
+
+struct Player{
+    char name[50];
+    int Runs_S, Runs_C;
+    int Balls_F, Balls_B;
+    int Wickets;
+    float Strike_R;
+    float Econ_R;
+};
+
+
+struct Match_Event{
+    int Wickets;
+    int Boundaries;
+    struct Player x;
+};
+
+
+struct Over{
+    struct Player x;
+    int over_num;
+    int Balls_B;
+    int Runs;
+    int Wickets;
+} Over;
+
+
+struct Match{
+    struct Team_1 T1;
+    struct Team_2 T2;
+    int Curr_Over;
+    int Curr_Runs;
+    struct Match_Event M_Event;
+    int Match_Results;
+} Match;
+
+
+struct Sponser{
+    char name[25];
+    char slogan[75];
+
+} Sponser;
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
